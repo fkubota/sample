4376eccfd5bb46e56645ffb9ddfead50ff2da98e
fkubota <rkkubotay@gmail.com>
2020-04-09 08:13:50 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
-// module.exports = {
-//   outputDir: "docs",
-//   assetsDir: "./",
-//   publicPath: "/"
-// };
-//
-
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/resume/'
-  : '/',
-  outputDir: 'docs',
-}
+  outputDir: "docs",
+  // assetsDir: "./",
+  publicPath: "/"
+};
+
+
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//   ? '/resume/'
+//   : '/',
+//   outputDir: 'docs',
+// }
